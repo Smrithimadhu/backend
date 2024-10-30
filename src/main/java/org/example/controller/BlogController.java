88382e23ff4e8394d64ced951e3251e3045c1660
Kiruppa <kiruppasri83@gmail.com>
2024-10-30 20:16:30 +0530

    I have modified the blogcontroller

--- src/main/java/org/example/controller/BlogController.java
+++ src/main/java/org/example/controller/BlogController.java
@@ -19,7 +19,6 @@ import java.util.List;
 public class BlogController {
 
     //-------------------------PROPERTIES------------------------------------------------------------------------
-    //Hello guys branch 1 .
     @Autowired
     private BlogService blogService;
     @Autowired
