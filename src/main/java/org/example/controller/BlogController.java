8e19d80ae3c13669e74ff4ad89876a1ef19d3214
Smrithimadhu <smrithi.madhu1603@gmail.com>
2024-11-01 15:46:06 +0530

    updated cors

--- src/main/java/org/example/controller/BlogController.java
+++ src/main/java/org/example/controller/BlogController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class BlogController {
